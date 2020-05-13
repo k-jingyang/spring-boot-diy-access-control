9556598547860b99debacc1668430f4c04fc279f
k-jingyang <k.jingyang@gmail.com>
2020-05-13 19:13:11 +0800

    Created voter that always fail

--- src/main/java/com/jingyang/accesscontrol/WebSecurityConfig.java
+++ src/main/java/com/jingyang/accesscontrol/WebSecurityConfig.java
@@ -1,14 +1,28 @@
 package com.jingyang.accesscontrol;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.jingyang.accesscontrol.domain.TestVoter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,13 +30,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/static/**").permitAll().anyRequest().hasRole("USER").and().formLogin();
+        http.authorizeRequests().antMatchers("/static/**").permitAll().and().authorizeRequests().anyRequest()
+                .hasRole("USER").accessDecisionManager(accessControlManager()).and().formLogin();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
     }
 
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.builder().username("saul").password("{noop}password").authorities("ROLE_USER").build();
+        UserDetails user1 = User.builder().username("saul").password("{noop}password").roles("USER").build();
         return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public AccessDecisionManager accessControlManager() {
+        // Use OPA voter here..
+        return new UnanimousBased(Lists.newArrayList(new WebExpressionVoter(), new TestVoter()));
     }
 }
